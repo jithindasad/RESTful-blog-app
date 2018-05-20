a1b9e8ed19d63aafe5460b6b3e476284f7da470c
jithindasad <jithindasad@gmail.com>
2018-05-20 20:03:08 +0530

    Add Create and New Route and Template

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.get("/", function(req, res){
   res.redirect("/blogs");
 });
 
+//INDEX Route
 app.get("/blogs", function(req, res){
   Blog.find({}, function(err, blogs){
     if(err){
@@ -33,6 +34,23 @@ app.get("/blogs", function(req, res){
       res.render("index", {blogs: blogs});
     }
   })
+});
+
+//NEW Route
+app.get("/blogs/new", function(req, res){
+  res.render("new");
+});
+
+//CREATE Route
+
+app.post("/blogs", function(req, res){
+  Blog.create(req.body.blog, function(err, newBlog) {
+    if (err) {
+      res.render("new");
+    }else {
+      res.redirect("/blogs");
+    }
+  });
 });
 
 
