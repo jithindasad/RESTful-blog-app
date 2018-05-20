b5b3a208bfdc7888ab6ed5afcee0b1822b29c1ac
jithindasad <jithindasad@gmail.com>
2018-05-20 17:24:15 +0530

    add blog model and setup app

---
+++ app.js
@@ -0,0 +1,42 @@
+var bodyParser = require("body-parser"),
+    mongoose   = require("mongoose"),
+    express    = require("express"),
+    app        = express();
+
+//APP CONFIG
+mongoose.connect("mongodb://localhost/restful_blog_app");
+app.set("view engine", "ejs");
+app.set(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+//MONGOOSE/MODEL CONFIG
+var blogSchema = mongoose.Schema({
+  title: String,
+  body: String,
+  image: String,
+  created: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+//RESTful ROUTES
+
+app.get("/", function(req, res){
+  res.redirect("/blogs");
+});
+
+app.get("/blogs", function(req, res){
+  Blog.find({}, function(err, blogs){
+    if(err){
+      console.log(err);
+    }else {
+      res.render("index", {blogs: blogs});
+    }
+  })
+});
+
+
+
+app.listen(3000, function(){
+  console.log("server is running on port 3000");
+});
